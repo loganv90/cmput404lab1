d2881ec07102c3839a8467e1c7bd77f3bd682ad5
Logan Vaughan <60831705+loganv90@users.noreply.github.com>
2023-01-11 20:19:46 -0700

    update python script

--- q2.py
+++ q2.py
@@ -1,3 +1,4 @@
 import requests
 print(requests.__version__)
 print(requests.get("https://www.google.com").text)
+print(requests.get("https://raw.githubusercontent.com/loganv90/cmput404lab1/main/q2.py").text)
